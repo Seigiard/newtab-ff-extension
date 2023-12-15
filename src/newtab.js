5e7a11b69e131f2a492fd404ff03c6317e2e1b40
Andrew Borisenko <seigiard@gmail.com>
2023-12-15 21:44:53 +0100

    Update the project: add dark theme, add fix

--- src/newtab.js
+++ src/newtab.js
@@ -1,4 +1,4 @@
-;(async function() {
+;(async function () {
   // Find out what the homepage is set to
   const homepageRaw = (await browser.browserSettings.homepageOverride.get({}))
     .value
@@ -9,9 +9,17 @@
 
   // When we navigate away from the homepage, navigate the top level instead
   const currTab = await browser.tabs.getCurrent()
-  browser.webNavigation.onBeforeNavigate.addListener(e => {
-    console.log(e)
-    if (e.tabId === currTab.id && e.frameId !== 0 && e.parentFrameId === 0) {
+  browser.webNavigation.onBeforeNavigate.addListener((e) => {
+    const isCurrentTab = e.tabId === currTab.id
+    const isNotMainFrame = e.frameId !== 0
+    const isNotHomepage = e.url.indexOf(homepageRaw) === -1
+
+    if (
+      isNotHomepage &&
+      isCurrentTab &&
+      isNotMainFrame &&
+      e.parentFrameId === 0
+    ) {
       window.location.href = e.url
     }
   })
